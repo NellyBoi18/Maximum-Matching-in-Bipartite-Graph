dd7e26d647f7cd90b281abfddce9fbed858c622f
Nelson Lu <nelsonlu3@gmail.com>
2023-04-12 23:55:35 -0500

    Cleanup

--- maxMatching.c
+++ maxMatching.c
@@ -4,13 +4,13 @@
 
 #define MAX_NODES 1000
 
-int n, m, q;
+int numNodesA, numNodesB, numEdges;
 int graph[MAX_NODES][MAX_NODES];
 int match[MAX_NODES];
 bool visited[MAX_NODES];
 
 bool bpm(int u) {
-    for (int v = 0; v < n; v++) {
+    for (int v = 0; v < numNodesB; v++) {
         if (graph[u][v] && !visited[v]) {
             visited[v] = true;
             if (match[v] < 0 || bpm(match[v])) {
@@ -24,53 +24,55 @@ bool bpm(int u) {
 
 int max_matching() {
     int count = 0;
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < numNodesB; i++) {
         match[i] = -1;
     }
-    for (int u = 0; u < m; u++) {
-        for (int j = 0; j < n; j++) {
+
+    for (int u = 0; u < numNodesA; u++) {
+        for (int j = 0; j < numNodesB; j++) {
             visited[j] = false;
         }
         if (bpm(u)) {
             count++;
         }
     }
+
     return count;
 }
 
 bool is_perfect_matching() {
-    return max_matching() == n;
+    return max_matching() == numNodesB;
 }
 
 int main() {
-    int t;
-    scanf("%d", &t);
+    int numInstances;
+    scanf("%d", &numInstances);
 
-    int maxMatchSize[t];
-    int perfect[t];
-    char match[3][3] = {"N", "Y"};
+    int maxMatchSize[numInstances];
+    int perfect[numInstances];
     int count = 0;
-    char output[t * 5];
 
-    while (t--) {
-        scanf("%d %d %d", &m, &n, &q);
+    while (numInstances--) {
+        scanf("%d %d %d", &numNodesA, &numNodesB, &numEdges);
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < numNodesA; i++) {
+            for (int j = 0; j < numNodesB; j++) {
                 graph[i][j] = 0;
             }
         }
 
-        while (q--) {
+        while (numEdges--) {
             int i, j;
+
             scanf("%d %d", &i, &j);
             i--;
             j--;
+
             graph[i][j] = 1;
         }
 
         int max_match = max_matching();
-        int is_perfect = (max_match == m);
 
         /* // Individual instance outputs
         printf("%d ", max_match);
@@ -87,7 +89,6 @@ int main() {
     }
 
     for (int i = 0; i < count; i++) {
-        //printf("%d %s\n", maxMatchSize[i][0], match[results[i][1]]);
         printf("%d ", maxMatchSize[i]);
         if (perfect[i]) {
             printf("Y\n");
